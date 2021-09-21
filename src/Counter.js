053ce40b8147c3b0465d381e97d73dfd97be7e6b
enbre <noahbrezel@gmail.com>
2021-09-21 15:55:06 -0700

    Gets counter adjustment and auto-date working

--- src/Counter.js
+++ src/Counter.js
@@ -1,18 +1,26 @@
 import React, {useState, useEffect} from 'react'
 
 function Counter() {
-   const [count, setCount] = useState(0);
-   const [adjust, setAdjust] = useState(0);
+   const [count, setCount] = useState(0),
+   [adjust, setAdjust] = useState(0),
+   [base, setBase] = useState(0);
+
 
    useEffect(() => {
-      document.title = `You clicked ${count} times`
-   }, [count, adjust])
+      document.querySelector("h3" ).innerHTML = `Count: ${count+base+adjust}`
+      console.log(`count: ${count}, base: ${base}, adjust: ${adjust}`)
+   }
+   ,[ count,adjust, base]
+   )
 
    return (
       <div className="Counter">
-         <h3>Count: {count}</h3>
-         <button onClick={()=>setCount(count - adjust)}>-</button>
-         <button onClick={()=>setCount(count + adjust)}>+</button>
+         <h3> Count: banana</h3>
+         <button onClick={()=>setCount(count - (base+adjust))}>-</button>
+         <button onClick={()=>setCount(count + (base+adjust))}>+</button>
+         <h3>Base unit: {base}</h3>
+         <button onClick={()=>setBase(base - 1)}>-</button>
+         <button onClick={()=>setBase(base + 1)}>+</button>
          <h3>Adjustment: {adjust}</h3>
          <button onClick={()=>setAdjust(adjust - 1)}>-</button>
          <button onClick={()=>setAdjust(adjust + 1)}>+</button>
