e22e1f74ee2b0450347a9c845a4e7b9a94099ce2
enbre <noahbrezel@gmail.com>
2021-10-08 16:21:45 -0700

    adds basic styling to header

--- src/Header.js
+++ src/Header.js
@@ -11,18 +11,20 @@ function Header(props) {
 
    return (
       <div className="Counter-header">
-      <>
-         <h1 id = "Total" >Total: {props.total} </h1>
-         <h2>Counters: {props.counterNum} </h2>
-         {/* <button onClick={props.decreaseCounters} disabled = {props.counterNum<1} >-</button> */}
-         <button onClick={closeCounter} disabled = {props.counterNum<1} >-</button>
-         <button onClick={props.increaseCounters}>+</button>
-      </>   
-      <>
-         <h2>Increment: {props.increment}</h2>
-         <button onClick={props.decreaseIncrement} disabled = {props.increment<2}>-</button>
-         <button onClick={props.increaseIncrement}>+</button>
-      </>
+         <div className="Counter-element">
+            <h2>Counters: &#160; {props.counterNum} </h2>
+            <br/>
+            <button onClick={closeCounter} disabled = {props.counterNum<1} >-</button>
+            <button onClick={props.increaseCounters}>+</button>
+         </div>   
+         <div className="Counter-element">
+            <h2>Increment: {props.increment}</h2>
+            <button onClick={props.decreaseIncrement} disabled = {props.increment<2}>-</button>
+            <button onClick={props.increaseIncrement}>+</button>
+         </div>
+         <div className="Counter-element">
+            <h2 id = "Total" >Total:{props.total} </h2>
+         </div>
       </div>
    )
 }
